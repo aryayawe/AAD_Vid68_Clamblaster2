0be7daea456ca09146babdef591cce4ae7f4daa3
Anna Billing <Anna Billing>
2016-03-20 23:40:03 +1300

    Initial commit

---
+++ app/src/test/java/com/annabilling/aad_vid68_clamblaster2/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.annabilling.aad_vid68_clamblaster2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
